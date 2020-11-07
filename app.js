ab9992c73e61a4a0f1a37a6d78cebeb8c02b85bf
Sureshvarma4599 <sureshvarma.n666@gmail.com>
2020-11-07 11:16:18 +0530

    completed

--- app.js
+++ app.js
@@ -23,30 +23,12 @@ function displayclock(){
   document.getElementById("demo1").innerHTML=hrs +" : "+ min;
 
 }
-/*
-  var msg=document.getElementById("img-1")
-  msg.addEventListener("click",message);
-  function message(){
-      document.getElementById("msggg").style.visibility="block";
-  }
-*/
+
 function myImg(){
   document.getElementById("demo").style.display="none";
   document.getElementById("demo").className="demo2";
   document.getElementById("demo2").style.display="block";
-}/*
-var par1=document.getElementById("p1");
-par1.addEventListener("click",myFun);
-function myFun(){
-  document.getElementById("p1").className="html";
-  document.getElementById("html").style.display="block";
-  document.getElementById("demo2").style.display="none";
-  document.getElementById("p1").style.display="none";
-  document.getElementById("p2").style.display="none";
-  document.getElementById("p3").style.display="none";
-  document.getElementById("demo2").style.display="none";
-  
-}*/
+}
 function pOne(){
   document.getElementById("p1").style.display="none";
   document.getElementById("p2").style.display="none";
